2057c10e19849c46b32e3a93b486f796419f0f10
sasikanth reddy <sasikanthreddypoli@gmail.com>
2025-12-24 10:45:38 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -1,6 +1,16 @@
+pattern of square
 x=int(input())
 for i in range(x):
     for j in range(x):
         print("*",end=" ")
     print()
+
+
+pattern of triangle
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+    
     
