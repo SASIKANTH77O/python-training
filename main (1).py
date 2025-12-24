2db6bbc6bdcdf0e450b5ddcc93b065a3be6e3b77
sasikanth reddy <sasikanthreddypoli@gmail.com>
2025-12-24 12:26:27 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -43,7 +43,7 @@ for i in range(x-2,-1,-1):
     for j in range(i+1):
         print("*",end=" ")
     print()
-    #hallow pattern 
+  #hollow pattern 
 x=int(input())
 for i in range(x+4):
     for j in range(x-i+4):
@@ -54,13 +54,32 @@ for i in range(x+4):
         else:
          print("*",end=" ")
     print()
-#hallow pattern 2
+#hollow pattern 2
 n=int(input())
 for i in range(n):
     for j in range(n-i-1):
         print(" ",end="")
     for j in range(2*i+1):
         print("*",end=" ")
+    print()
+
+#hollow square pattern
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or j==n-1:
+            print("*",end=" ")
+        else:
+          print(" ",end=" ")
+    print()
+#hollow right angle triangle pattern
+n=int(input())
+for i in range(n):
+    for j in range(i+1):
+        if i==n-1 or j==0 or i ==j:
+            print("*",end=" ")
+        else:
+          print(" ",end=" ")
     print()
 
     
